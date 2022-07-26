8577e15286f3b0bf97b831a093f665499ed13eca
Jason Martinez <jasonmar08@gmail.com>
2022-07-26 02:57:39 -0400

    added scripts

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const db = require('./db')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
+
+app.use('/', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error!'))
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
