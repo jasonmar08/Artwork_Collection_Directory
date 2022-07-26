ec563e37f349218ebe793e1dad0241abc31841ad
Jason Martinez <jasonmar08@gmail.com>
2022-07-26 02:27:02 -0400

    created components and pages and imports in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg'
 import './App.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import CollectionPage from './pages/CollectionPage'
 
 function App() {
   return <div className="App"></div>
