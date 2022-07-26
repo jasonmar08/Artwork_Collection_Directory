d103616916ae47df05a5e1280ee15e3ac4b9f07b
Jason Martinez <jasonmar08@gmail.com>
2022-07-26 02:45:50 -0400

    installed nodemon, cors, morgan, and imported in server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
